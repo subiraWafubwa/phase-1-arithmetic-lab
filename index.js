79312971e71a0b9863f95c82f9bb68bca408fb59
Subira Wafubwa <subira.craig.2023@gmail.com>
2024-06-24 22:11:30 +0300

    Last Commit

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+// Define num1 and num2 such that their product is 62
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+// Generate a random integer greater than 0
+const random = Math.floor(Math.random() * 100) + 1;
+
+// Define num3 and num4 such that their remainder is 4 when num3 is divided by num4
+const num3 = 28;
+const num4 = 6;
+const mod = num3 % num4;
+
+// Define a set of numbers and return the highest number
+const max = Math.max(1, 5, 10, 20);
